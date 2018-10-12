1876974ad89809d212ebf0d1b3083313b13ff768
APthom <pathoma58@email.nu.ac.th>
2018-10-12 09:10:53 +0000

    fix

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 8000
+var express = require('express');
+//var https = require('https');
+var httpPort = process.env.httpPort || 8000;
+//var httpsPort = process.env.httpsPort || 443;
 
+var app = express();
 
-app.use(express.static(path.join(__dirname,'resume')));
+app.use(express.static('resume'))
 
-app.get('/*',function(req,res){
-        res.sendFile(path.join(__dirname,'resume','index.html'))
+//app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname, 'resume', 'index.html'));
 });
 
-
-app.listen(port, () => console.log('app listening on port'+port))
+if (require.main === module) {
+  app.listen(httpPort, function() {
+    console.log('Server http listening on port %d', this.address().port);
+  });
+}
